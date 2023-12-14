406eefeafa02b12236b5afb7a6f560c3cd28f359
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-13 21:48:02 -0300

    Admin dashboard display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Home from "./Views/Home"
 import NavBar from "./Components/Home/NavBar"
+import Admin from "./Views/Admin"
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/admin" element={<Admin/>}/>
         </Routes>
       </div>
   )
---
+++ client/src/Views/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>SOY DASH</h1>
+    </div>
+  )
+}
+
+export default Admin
