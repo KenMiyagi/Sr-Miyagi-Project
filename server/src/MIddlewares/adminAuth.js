8971295f99dca4fe1365c6698ffd2b7c9c951d59
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-17 16:46:50 -0300

    Admin authorization middleware bug was fixed 2

--- server/src/MIddlewares/adminAuth.js
+++ server/src/MIddlewares/adminAuth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const adminAuthMiddleware = (req, res, next) =>{
     try {
         const authHeaders = req.headers["auth"]
-        const token = authHeaders && authHeaders.split(" ")[1].trim()
+        const token = (authHeaders && authHeaders.includes("Bearer")) ? authHeaders.split(" ")[1].trim() : authHeaders.trim()
         if(!token) return res.status(401).json({error:"Not logged in."})
 
         jwt.verify(token, process.env.SIGNATURE, (err,user)=>{
